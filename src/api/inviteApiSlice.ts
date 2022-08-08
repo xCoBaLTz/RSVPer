d0db904256b7f77b855573a0828901c799bc58fa
Seanan Chabra <seanan.chabra@freshbooks.com>
2022-08-07 22:18:57 -0400

    invite api slice

---
+++ src/api/inviteApiSlice.ts
@@ -0,0 +1,30 @@
+import apiSlice from "./apiSlice";
+import { Invite } from "../redux/interfaces/inviteState";
+
+export const inviteApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getInvites: builder.query<Invite[], void>({
+      query: () => "/invites",
+      providesTags: (result, error, arg) =>
+        result
+          ? [
+              ...result.map(({ id }) => ({ type: "Invite" as const, id })),
+              "Invite",
+            ]
+          : ["Invite"],
+    }),
+    updateInvite: builder.mutation<
+      Invite,
+      Partial<Invite> & Pick<Invite, "id">
+    >({
+      query: (body) => ({
+        url: `/invites/${body.id}`,
+        method: "PUT",
+        body,
+      }),
+      invalidatesTags: (result, error, arg) => [{ type: "Invite", id: arg.id }],
+    }),
+  }),
+});
+
+export const { useGetInvitesQuery, useUpdateInviteMutation } = inviteApiSlice;
